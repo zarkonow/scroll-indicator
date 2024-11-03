fa56e17bc92b3a6c172b8ebc68c4dacea83dfb72
zarkonow <stojanov1987@gmail.com>
2024-11-03 23:03:05 +0100

    *added how much scrolled

--- src/scroll-indicator/index.jsx
+++ src/scroll-indicator/index.jsx
@@ -4,6 +4,7 @@ export default function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setloading] = useState(false);
   const [errorMessage, setErrormessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   async function fetchData(getUrl) {
     try {
@@ -17,13 +18,38 @@ export default function ScrollIndicator({ url }) {
       }
     } catch (e) {
       console.log(e);
-      setErrormessage(e.Message);
+      setErrormessage(e.message);
     }
   }
 
   useEffect(() => {
     fetchData(url);
   }, [url]);
+
+  function handleScrollPercentage() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+
+    const scrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const heigth =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+      setScrollPercentage((scrolled/ heigth)*100); 
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
 
   console.log(data, loading);
 
@@ -32,7 +58,7 @@ export default function ScrollIndicator({ url }) {
       <h1> Custom Scroll Indicator</h1>
       <div className="data-container">
         {data && data.length > 0
-          ? data.map((dataItem) => <p>{DataTransferItem.title}</p>)
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
           : null}
       </div>
     </div>
